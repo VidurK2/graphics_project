d88839f318b737ae55a24a2d42245b0638aa3ef4
Vidur Kaushik <54384846+VidurK2@users.noreply.github.com>
2024-04-13 00:11:18 +0530

    restarted and changed base cide

--- gravity_sim.py
+++ gravity_sim.py
@@ -1,138 +1,226 @@
+import numpy
 import pygame
-import numpy as np
+import random
+import time
 
-main_color = (255, 255, 255)
-wall_color = (120, 238, 74)
+pygame.init()
+
+blockColor = (50, 85, 140)
+screenColor = (255,255,255)
+lineColor = (0,0,0)
+shCol = (100, 100, 100)
+
 total_box_width = 500
 total_box_height = 500
+thickness_wall = 5
+blocks_no = 0
+playing = True
+times = pygame.time.Clock()
+screen = pygame.display.set_mode((800, 600))
+blocks = []
+isPause = False
+
 up_side = 50
 down_side = up_side + total_box_height
 left_side = 100
 right_side = left_side + total_box_width
-thickness_wall = 5
-blocks = []
 
-block_color = (50, 85, 140)
-block_width = 1
+collision_left = pygame.Rect(left_side, up_side, thickness_wall, total_box_height)
+collision_right = pygame.Rect(right_side, up_side, thickness_wall, total_box_height + thickness_wall)
+collision_up = pygame.Rect(left_side, up_side, total_box_width, thickness_wall)
+collision_down = pygame.Rect(left_side, down_side, total_box_width, thickness_wall)
 
-height = 800
-width = 600
+shapes = []
+sizeBlockNew = 1
+collide = True
+blocksNew = 1
+newBlockDraw = "Blocks"
+click = False
+prevKeyTime = 0
+prevMousePointer = (0, 0)
+isPause = True
+clockTick = 1.0
 
-pygame.init()
-screen = pygame.display.set_mode((height, width))
-
-
-
-gravity = False
-
-def block_draw(block):
-    circle = pygame.draw.circle(screen, color_block, (block.x, block.y), block.radius, width_block)
-    return circle
-
-# run
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pressed_key = pygame.key.get_pressed()
+""" gravity = False    
+g_scale = 0  
+self.last_shape_position = (0, 0)
+self.boxes = []
+self.circular_motion = False
+self.centripetal_acceleration = 0 """
 
 
-    if pressed_key[pygame.K_RETURN] and not pressed_key[pygame.K_LSHIFT]:
-        if gravity:
-            gravity = False
-        else:
-            gravity = True
+
+def move(block):
+
+    block['posX'] += block['speedX'] * clockTick
+    block['posY'] += block['speedY'] * clockTick
+
+def wallHit(rect, block):
+
+    # collision with walls
+    if rect.colliderect(collision_left):
+        block['posX'] += 2 
+        block['speedX'] = -block['speedX'] * 1
+        block['pastCollide'] = collision_left
+
+    elif rect.colliderect(collision_right):
+        block['posX'] -= 2 
+        block['speedX'] = -block['speedX'] * 1
+        block['pastCollide'] = collision_right
+
+    elif rect.colliderect(collision_up):
+        block['posY'] += 2 
+        block['speedY'] = -block['speedY'] * 1
+        block['pastCollide'] = collision_up
+
+    elif rect.colliderect(collision_down):
+        block['posY'] -= 2 
+        block['speedY'] = -block['speedY'] * 1
+        block['pastCollide'] = collision_down
+
+def set_random_position(block):
+    posX = random.randint(left_side + block['radius'], right_side - block['radius'])
+    posY = random.randint(up_side + block['radius'], down_side - block['radius'])
+    drawCircle = pygame.draw.circle(screen, blockColor, (posX, posY), block['radius'])
+
+def blockCreate(box, mass, location=None, speedX=None, speedY=None):
+    radius = mass * 5
+
+    if speedX is None:
+        speedX = random.random() * random.randint(-2, 2)
     
-    screen.fill(main_color)
-    pygame.display.set_caption("Gravity Simulation")
-    collision_left = pygame.Rect(left_side, up_side, thickness_wall, total_box_height)
-    collision_right = pygame.Rect(right_side, up_side, thickness_wall, total_box_height + thickness_wall)
-    collision_up = pygame.Rect(left_side, up_side, total_box_width, thickness_wall)
-    collision_down = pygame.Rect(left_side, down_side, total_box_width, thickness_wall)
+    else:
+        speedX = speedX
 
-    pygame.draw.rect(screen, wall_color, collision_left)
-    pygame.draw.rect(screen, wall_color, collision_right)
-    pygame.draw.rect(screen, wall_color, collision_up)
-    pygame.draw.rect(screen, wall_color, collision_down)
+    if speedY is None:
+        speedY = random.random() * random.randint(-2, 2)
+    
+    else:
+        speedY = speedY
+    
+    if not location:
+        posX = random.randint(left_side + radius, right_side - radius)
+        posY = random.randint(up_side + radius, down_side - radius)
+    
+    else:
+        posX, posY = location[0], location[1]
+    
+    drawCircle = pygame.draw.circle(screen, blockColor, (posX, posY), radius)
+    pastCollide = None
 
-    for block in blocks:
-        if not paused:
-            block.move()
-        sq = block_draw(block)
-        block.sq = sq
-        block.check_collide()
+    return {
+        'box': box,
+        'mass': mass,
+        'radius': radius,
+        'speedX': speedX,
+        'speedY': speedY,
+        'posX': posX,
+        'posY': posY,
+        'drawCircle': drawCircle,
+        'pastCollide': pastCollide
+    }
 
-    pygame.display.flip()
+def blockAdd(block=None):
+    global blocks_no
+
+    if not block:
+        block = blockCreate(screen, sizeBlockNew)
+
+    disPos = False
+
+    blocks.append(block)
+    blocks_no += 1
 
 
-class block:
-    def __init__(self, x, y, radius, color, width):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.width = width
-        self.vx = 0
-        self.vy = 0
-        self.sq = None
+def play():
+    global playing, collide, blocksNew, sizeBlockNew, newBlockDraw, click, prevKeyTime, prevMousePointer, isPause
+    while playing:
+        times.tick(240)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+            
+            key = pygame.key.get_pressed()
 
-    def move(self):
-        self.x += self.vx
-        self.y += self.vy
-        if gravity:
-            self.vy += 0.1
+            if key[pygame.K_BACKSPACE]:
+                print("Pressed Backspace", collide)
+                if collide:
+                    collide = False
+                else:
+                    collide = True
 
-    def check_collide(self):
-        if self.sq.colliderect(collision_left) or self.sq.colliderect(collision_right):
-            self.vx *= -1
-        if self.sq.colliderect(collision_up) or self.sq.colliderect(collision_down):
-            self.vy *= -1
+            if key[pygame.K_p]:
+                print("Pressed P", newBlockDraw)
+                newBlockDraw = "Blocks"
+            
+            if key[pygame.K_LEFT]:
+                if blocksNew != 1:
+                    blocksNew //= 2
+            
+            if key[pygame.K_RIGHT]:
+                if blocksNew != 64:
+                    blocksNew = blocksNew * 2
+            
+            if key[pygame.K_UP]:
+                if sizeBlockNew < 5:
+                    sizeBlockNew = sizeBlockNew + 1
+            
+            if key[pygame.K_DOWN]:
+                if sizeBlockNew > 1:
+                    sizeBlockNew = sizeBlockNew - 1
+            
+            """ if key[pygame.K_ESCAPE]:
+                  reset()  """    
 
-    def draw(self):
-        circle = pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.width)
-        return circle
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                    click = True
 
-    def set_velocity(self, vx, vy):
-        self.vx = vx
-        self.vy = vy
+            if event.type == pygame.MOUSEBUTTONUP:
+                    click = False
+            
+            if key[pygame.K_SPACE]:
+                print(isPause)
+                if time.monotonic() - prevKeyTime > 0.1:
+                    prevKeyTime = time.monotonic()
+                    if isPause:
+                        isPause = False
+                    else:
+                        isPause = True
 
-    def set_position(self, x, y):
-        self.x = x
-        self.y = y
+            if click:
+                if newBlockDraw == "Blocks":
+                    if blocksNew == 1:
+                        new_x, new_y = pygame.mouse.get_pos()
+                        prev_x, prev_y = prevMousePointer
+                        if abs(new_x - prev_x) > 15 or abs(new_y - prev_y) > 15:
+                            blockAddBlock = blockCreate(screen, sizeBlockNew, (new_x, new_y))
+                            blockAdd(blockAddBlock)
+                            prevMousePointer = new_x, new_y
+                    else:
+                        for i in range(blocksNew):
+                            blockAddScreen = blockCreate(screen, sizeBlockNew)
+                            screen.blockAdd(blockAddScreen)
+            
+            screen.fill(screenColor)
 
-    def set_color(self, color):
-        self.color = color
+            pygame.draw.rect(screen, lineColor, collision_left)
+            pygame.draw.rect(screen, lineColor, collision_right)
+            pygame.draw.rect(screen, lineColor, collision_up)
+            pygame.draw.rect(screen, lineColor, collision_down)
+        
 
-    def set_radius(self, radius):
-        self.radius = radius
+            # draw_text()
 
-    def set_width(self, width):
-        self.width = width
+            for block in blocks:
+                if not isPause:
+                    move(block)
+                
+                rect = pygame.draw.circle(screen, blockColor, (block['posX'], block['posY']), block['radius'], 1)
 
-    def set_square(self, sq):
-        self.sq = sq
+                block['rect'] = rect
+                wallHit(rect, block)
 
-    def get_velocity(self):
-        return self.vx, self.vy
+            pygame.display.flip()
+    pygame.quit()
 
-    def get_position(self):
-        return self.x, self.y
-
-    def get_color(self):
-        return self.color
-
-    def get_radius(self):
-        return self.radius
-
-    def get_width(self):
-        return self.width
-
-    def get_square(self):
-        return self.sq
-
-    def __str__(self):
-        return f"Block at ({self.x}, {self.y}) with radius {self.radius} and color {self.color}"    
-
-pygame.quit()
+play()
